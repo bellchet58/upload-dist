b85760f5070559b643267390cd1011d3fdfe1dae
liubj <liubj@belewtech.com>
2018-11-15 17:30:25 +0800

    处理文件环境的问题

--- bin/upload-dist.js
+++ bin/upload-dist.js
@@ -7,7 +7,7 @@ var package = require('../package.json')
 program.version('v' + package.version)
   .description('check the tool\'s version')
 
-program.command('sync <uploadUrl> <relativeDir>')
+program.command('sync <uploadUrl> [relativeDir]')
   .alias('s')
   .description('sync webpacked dist dir to url')
   .action(function (uploadUrl, relativeDir) {
