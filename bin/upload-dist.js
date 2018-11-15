9e59e5fb5157f34c3e6927fb32b1f935d7ece6a0
liubj <liubj@belewtech.com>
2018-11-15 16:36:19 +0800

    添加commander

---
+++ bin/upload-dist.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+var uploader = require('../lib/uploader')
+var program = require('commander')
+var chalk = require('chalk')
+var package = require('../package.json')
+
+program.version('v' + package.version)
+  .description('check the tool\'s version')
+
+program.command('sync <uploadUrl> <relativeDir>')
+  .alias('s')
+  .description('sync webpacked dist dir to url')
+  .action(function (uploadUrl, relativeDir) {
+    uploader.doUpload(uploader.getFormData(relativeDir), uploadUrl).then(() => {
+      console.log(chalk.green('upload success!'))
+    }).catch(err => {
+      console.error(chalk.red('upload failed!\n'), chalk.blue(err))
+    })
+  })
+
+program.parse(process.argv)
+
+if (program.args.length === 0) {
+  program.help()
+}
