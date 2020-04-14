ce2c398996a04dd2a9b30566bef105bb3180aed8
liubj <liubj@belewtech.com>
2020-04-14 14:58:27 +0800

    添加gradlew的功能及其说明

--- README.md
+++ README.md
@@ -5,10 +5,14 @@ using axios to upload packed dist directory
 1. 进入项目文件夹
 2. yarn add --dev upload-dist (npm install --save-dev upload-dist)
 
-### sync <uploadUrl> [relativeDir] [--addConf [projectDomain]]
-  用于向uploadUrl上传relativeDir文件夹中的内容，如果relativeDir没有指定，则默认为dist文件夹。如果有`addConf`可选项的话，则添加relativeDir项目的服务器（目前暂时是Apache）配置文件，并重启静态服务器。
-  如果没有指定projectDomain的话，默认域名是`${relativeDir}.com`
+### sync <uploadUrl> [relativeDir] [--addConf [projectDomain]] [--gradlew [projectPath]]
+  1. 用于向uploadUrl上传relativeDir文件夹中的内容，如果relativeDir没有指定，则默认为dist文件夹。如果有`addConf`可选项的话，则添加relativeDir项目的服务器（目前暂时是Apache）配置文件，并重启静态服务器。
+  2. 如果没有指定projectDomain的话，默认域名是`${relativeDir}.com`
+  3. 若有`gradlew`可选项的话，则会生成可以下载打包生成的APK的下载地址
+  4. 若要用`gradlew`打包功能，`uploadUrl`必须是用`http://xxxxxxxxxx:12345/uploadTo?path=${打包的工程项目地址的绝对路径}`；此操作会在项目地址下递归创建`app/src/main/assets/apps/belewtech/www`前端资源文件夹，并覆盖要上传的文件夹(`dist文件夹`)
+
 
 #### 使用
 1. 在项目文件夹添加中的scripts部分添加命令。
-2. `upload-dist sync http://xxxxxxxxxx:12345/upload/meso_index --addConf meso.belewtech.com`
+2. `upload-dist sync http://xxxxxxxxxx:12345/upload/meso_index --addConf meso.belewtech.com`, 上传`dist`文件夹至服务器项目下`public/meso_index`文件夹，同时拷贝至`/var/www`下并生成`ubuntu apache2`配置文件
+3. `upload-dist sync http://xxxxxxxxxx:12345/uploadTo?path=/root/juanrong/UBcy --gradlew`, 上传`dist`文件夹至服务器`/root/juanrong/UBcy`项目下的前端资源文件夹（见上文提到的）,并生成下载地址
--- bin/upload-dist.js
+++ bin/upload-dist.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var uploader = require('../lib/uploader')
 var confController = require('../lib/confController')
+var gradlewHelper = require('../lib/gradlewHelper')
 var program = require('commander')
 var chalk = require('chalk')
 var package = require('../package.json')
@@ -8,6 +9,7 @@ var package = require('../package.json')
 program.version('v' + package.version)
   .description('check the tool\'s version')
   .option('-a, --addConf [projectDomain]', 'add static server(apache/nginx etc.) conf to target machine and restart server')
+  .option('-g, --gradlew [projectPath]', 'generate apk file using gradlew')
 
 program.command('sync <uploadUrl> [relativeDir]')
   .alias('s')
@@ -16,10 +18,14 @@ program.command('sync <uploadUrl> [relativeDir]')
     uploader.doUpload(uploader.getFormData(relativeDir), uploadUrl).then(() => {
       console.log(chalk.green('upload success!'))
       cmd.addConf = program.addConf
+      cmd.gradlewPath = program.gradlew
       if (cmd.addConf) {
         return confController.createApacheConf({ projectDomain: typeof cmd.addConf === 'boolean' ? null : cmd.addConf, uploadUrl }).then(resp => {
           resp.data.success ? console.log(chalk.green('conf create success!')) : console.log(chalk.red('conf create failed!\n Try Again!'), chalk.blue(resp.data.msg))
         })
+      }
+      if(cmd.gradlewPath) {
+        console.log(chalk.green('open'), chalk.yellow(`${gradlewHelper.getUrl({ projectPath: typeof cmd.gradlewPath === 'boolean' ? null: cmd.gradlewPath, uploadUrl })}`), chalk.green('in the browser to download!'))
       }
     }).catch(err => {
       console.error(chalk.red('upload failed!\n'), chalk.blue(err))
